bee2500d9082d90d77bd87e370b62430d4a10e95
vitaliy <zndev@ya.ru>
2023-04-12 11:17:47 +0600

    upd

--- src/Infrastructure/DependencyInjection/Symfony/services/validator.php
+++ src/Infrastructure/DependencyInjection/Symfony/services/validator.php
@@ -3,6 +3,8 @@
 use Psr\Container\ContainerInterface;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Untek\Model\Validator\Libs\Validators\ChainValidator;
+use Symfony\Component\Validator\ValidatorBuilder;
+use Untek\Model\Validator\Interfaces\ValidatorInterface;
 
 use function Symfony\Component\DependencyInjection\Loader\Configurator\service;
 
@@ -10,7 +12,8 @@ return static function (ContainerConfigurator $configurator): void {
     $services = $configurator->services();
     $parameters = $configurator->parameters();
 
-    $services->set(\Symfony\Component\Validator\ValidatorBuilder::class, \Symfony\Component\Validator\ValidatorBuilder::class);
+    $services->set(ValidatorBuilder::class, ValidatorBuilder::class);
+    $services->alias(ValidatorInterface::class, ChainValidator::class);
     $services->set(ChainValidator::class, ChainValidator::class)
         ->args(
             [
