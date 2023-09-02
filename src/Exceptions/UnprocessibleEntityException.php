84845663e8aa8ca8909783be89970a42d6b58928
vitaliy <theyamshikov@yandex.ru>
2023-09-02 17:05:10 +0600

    upd

--- src/Exceptions/UnprocessibleEntityException.php
+++ src/Exceptions/UnprocessibleEntityException.php
@@ -9,7 +9,7 @@ use Untek\Core\Collection\Interfaces\Enumerable;
 use Untek\Core\Collection\Libs\Collection;
 use Untek\Model\Validator\Entities\ValidationErrorEntity;
 
-DeprecateHelper::softThrow();
+//DeprecateHelper::softThrow();
 
 /**
  * Class UnprocessibleEntityException
