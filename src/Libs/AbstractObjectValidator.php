c14dc0f1c8abc19eaf8815398e83906d7a167a02
Vitaliy <theyamshikov@yandex.ru>
2024-03-12 17:48:43 +0500

    upd

--- src/Libs/AbstractObjectValidator.php
+++ src/Libs/AbstractObjectValidator.php
@@ -7,12 +7,19 @@ use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 use Symfony\Component\Serializer\Serializer;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintViolationList;
+use Symfony\Component\Validator\Context\ExecutionContext;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 use Symfony\Component\Validator\ValidatorBuilder;
+use Symfony\Contracts\Translation\TranslatorInterface;
 use Untek\Model\Validator\Exceptions\UnprocessableEntityException;
 
 abstract class AbstractObjectValidator
 {
+
+    public function __construct(private ?TranslatorInterface $translator = null)
+    {
+    }
 
     abstract public function getConstraint(): Constraint;
 
@@ -22,16 +29,26 @@ abstract class AbstractObjectValidator
      */
     public function validate(object $object): void
     {
-        $validator = $this->createValidator();
-        $serializer = $this->getSerializer();
-        $array = $serializer->normalize($object);
-        $constraints = $this->getConstraint();
-        $violations = $validator->validate($array, $constraints);
+        $value = $this->getSerializer()->normalize($object);
+        $violations = $this->validateObject($value, $this->getConstraint());
         if ($violations->count()) {
             $exception = new UnprocessableEntityException();
             $exception->setViolations($violations);
             throw $exception;
         }
+    }
+
+    protected function validateObject($value, $constraints = null, $groups = null): ConstraintViolationList
+    {
+        $validator = $this->createValidator();
+        if (isset($this->translator)) {
+            $contextualValidator = $validator->inContext(new ExecutionContext($validator, null, $this->translator));
+            $contextualValidator->validate($value, $constraints, $groups);
+            $violations = $contextualValidator->getViolations();
+        } else {
+            $violations = $validator->validate($value, $constraints, $groups);
+        }
+        return $violations;
     }
 
     private function createValidator(): ValidatorInterface
