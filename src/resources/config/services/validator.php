f5c984a1faeb264f84cd5fe9e6fffa2d20c3f386
Vitaliy <theyamshikov@yandex.ru>
2024-08-09 15:50:39 +0500

    upd

--- src/resources/config/services/validator.php
+++ src/resources/config/services/validator.php
@@ -12,7 +12,7 @@ return static function (ContainerConfigurator $configurator): void {
     $services = $configurator->services()->defaults()->public();
     $parameters = $configurator->parameters();
 
-    $services->set(ValidatorBuilder::class, ValidatorBuilder::class);
+//    $services->set(ValidatorBuilder::class, ValidatorBuilder::class);
     $services->alias(ValidatorInterface::class, ChainValidator::class);
     $services->set(ChainValidator::class, ChainValidator::class)
         ->args(
